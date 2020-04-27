4106e8ba92440b3546bf5e586526c975a67185fd
Chanvin Xiao <chanvinxiao@163.com>
2020-04-27 10:30:02 +0800

    Add Prefetch

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'preload.html'
     }),
-    new PreloadWebpackPlugin()
+    new HtmlWebpackPlugin({
+      filename: 'prefetch.html'
+    }),
+    new PreloadWebpackPlugin({
+      excludeHtmlNames: ['prefetch.html']
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'prefetch',
+      excludeHtmlNames: ['preload.html']
+    })
   ]
 }
